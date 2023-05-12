80574397badb5ef36e64adbdc7a71ddccec7d7cc
Svyatoslav Shypka <svyat200909@gmail.com>
2023-05-12 11:12:19 +0200

    Initial

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,29 @@
+public class BinarySearchTree<T extends Comparable<T>> {
+    public void add(T value) throws DuplicateElementException {
+        // TODO: Dodawanie nowej wartości do drzewa. Rzuć DuplicateElementException, jeśli element już istnieje.
+    }
+
+    public boolean contains(T value) {
+        // TODO: Sprawdzenie, czy drzewo zawiera podaną wartość.
+        return false;
+    }
+
+    public void delete(T value) {
+        // TODO: Usunięcie wskazanej wartości z drzewa.
+    }
+
+    public String toStringPreOrder() {
+        // TODO: Zwróć wartość String reprezentującą drzewo po przejściu metodą pre-order.
+        return null;
+    }
+
+    public String toStringInOrder() {
+        // TODO: Zwróć wartość String reprezentującą drzewo po przejściu metodą in-order.
+        return null;
+    }
+
+    public String toStringPostOrder() {
+        // TODO: Zwróć wartość String reprezentującą drzewo po przejściu metodą in-order.
+        return null;
+    }
+}
