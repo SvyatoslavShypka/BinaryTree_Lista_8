40ef3516621672e609ce2f22722cd76d9067053d
Svyatoslav Shypka <svyat200909@gmail.com>
2023-05-15 11:32:03 +0200

    Initial Finished

--- src/BinarySearchTreeSorter.java
+++ src/BinarySearchTreeSorter.java
@@ -1,20 +1,14 @@
-import java.util.LinkedList;
 import java.util.List;
 
 public class BinarySearchTreeSorter {
     public static <T extends Comparable<T>> void sort(List<T> list) throws DuplicateElementException {
         // TODO: Posortuj listę używając klasy BinarySearchTree.
-        System.out.println("list.toStringInOrder() = " + list.toString());
         BinarySearchTree<T> newList = new BinarySearchTree<T>();
         for (int i = 0; i < list.size(); i++) {
             newList.add(list.get(i));
         }
-        List<T> resultList = new LinkedList<>();
-        resultList = sortTraverseInOrder(newList.root, resultList);
         list.clear();
-        for (int i = 0; i < resultList.size(); i++) {
-            list.add(resultList.get(i));
-        }
+        list = sortTraverseInOrder(newList.root, list);
     }
     private static <T extends Comparable<T>> List<T> sortTraverseInOrder(BinarySearchTree.Node node, List<T> resultList) {
         if (node != null) {
